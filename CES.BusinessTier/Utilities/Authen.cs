cd5a5d8259aebf22cc9f32e1a97ed06a07fac71b
Huy Nguyen Anh <93422806+huy-nguyenanh@users.noreply.github.com>
2023-06-09 22:11:40 +0700

    Update Authen.cs

--- CES.BusinessTier/Utilities/Authen.cs
+++ CES.BusinessTier/Utilities/Authen.cs
@@ -30,7 +30,7 @@ namespace CES.BusinessTier.Utilities
                     new Claim(ClaimTypes.MobilePhone, account.Phone),
                     new Claim("TokenId", Guid.NewGuid().ToString())
                 }),
-                Expires = DateTime.UtcNow.AddHours(3000),
+                Expires = DateTime.UtcNow.AddDays(7),
                 SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
             };
             var token = tokenHandler.CreateToken(tokenDescriptor);
