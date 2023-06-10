ef60148206f69209c53994e5943c38384a6856e2
Lê Anh Khoa (Kieran) <lakhoa2806@gmail.com>
2023-06-10 23:52:49 +0700

    feat: import-download excel account

--- CES.API/Controllers/ExcelController.cs
+++ CES.API/Controllers/ExcelController.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 using CES.BusinessTier.ResponseModels;
 using CES.BusinessTier.Services;
 using CES.DataTier.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using OfficeOpenXml;
@@ -24,118 +25,30 @@ namespace CES.API.Controllers
         {
             _excelService = excelService;
         }
-        
-        // GET: api/Excel
-        [HttpGet]
-        public IEnumerable<string> Get()
-        {
-            return new string[] { "value1", "value2" };
-        }
 
-        // GET: api/Excel/5
-        [HttpGet("{id}", Name = "Get")]
-        public string Get(int id)
-        {
-            return "value";
-        }
-
-        // POST: api/Excel
-        [HttpPost]
+        /// <summary>
+        /// Download Excel Template for Employee (Supplier, Employee can't use)
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet("account/template")]
         [Consumes("multipart/form-data")]
-        public async Task<IActionResult> UploadFile(IFormFile file)
+        public IActionResult UploadFile()
         {
-            try
-            {
-                if (file != null && file.Length > 0)
-                {
-                    var fileName = Guid.NewGuid().ToString() + Path.GetExtension(file.FileName);
-
-                    var filePath = Path.Combine("uploads", fileName);
-
-                    var directory = Path.GetDirectoryName(filePath);
-                    Directory.CreateDirectory(directory);
-
-                    using (var stream = new FileStream(filePath, FileMode.Create))
-                    {
-                        await file.CopyToAsync(stream);
-                    }
-
-                    ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
-                    using (ExcelPackage package = new ExcelPackage())
-                    {
-                        // var worksheet = package.Workbook.Worksheets[0];
-                        // var records = new List<Person>();
-                        // for (int row = 2; row <= worksheet.Dimension.End.Row; row++)
-                        // {
-                        //     var name = worksheet.Cells[row, 1].Value?.ToString();
-                        //     var age = int.Parse(worksheet.Cells[row, 2].Value?.ToString() ?? "0");
-                        //     var sex = worksheet.Cells[row, 3].Value?.ToString();
-                        //     var newPerson = new Person()
-                        //     {
-                        //         Name = name,
-                        //         Age = age,
-                        //         Sex = sex
-                        //     };
-                        //     records.Add(newPerson);
-                        // }
-                        ExcelWorksheet ws = package.Workbook.Worksheets.Add("Company");
-                        ws.Cells["A1"].Value = "Company Name:";
-                        ws.Cells["A1"].Style.Font.Bold = true;
-                        ws.Cells["A1"].Style.Font.Size = 16;
-                        ws.Cells["A1"].Style.VerticalAlignment = ExcelVerticalAlignment.Center;
-                        ws.Cells["A1"].Style.HorizontalAlignment = ExcelHorizontalAlignment.Center;
-                        
-                        // Auto-fit columns for better readability
-                        ws.Cells.AutoFitColumns();
-
-                        // var memoryStream = new MemoryStream();
-                        // package.SaveAs(memoryStream);
-                        //
-                        // var response = new HttpResponseMessage(HttpStatusCode.OK);
-                        // response.Content = new ByteArrayContent(memoryStream.ToArray());
-                        // response.Content.Headers.ContentDisposition = new ContentDispositionHeaderValue("attachment")
-                        // {
-                        //     FileName = "data.xlsx" // Specify the desired file name
-                        // };
-                        // response.Content.Headers.ContentType = new MediaTypeHeaderValue("application/vnd.openxmlformats-officedocument.spreadsheetml.sheet");
-                        //
-                        // return Ok(response);
-                        
-                        var stream = new MemoryStream(package.GetAsByteArray());
-
-                        return new FileStreamResult(stream, "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet")
-                        {
-                            FileDownloadName = "data.xlsx" // Specify the desired file name
-                        };
-                    }
-                }
-
-                return BadRequest("No file was upload");
-            }
-            catch (Exception e)
-            {
-                return BadRequest($"No file was upload {e}");
-            }
+            return _excelService.DownloadEmployeeTemplate();
         }
 
-        [HttpPost("import")]
+        /// <summary>
+        /// Import Employees from template (Supplier, Employee can't use)
+        /// </summary>
+        /// <param name="file"></param>
+        /// <returns></returns>
+        [Authorize(Roles = "Enterprise Admin, System Admin")]
+        [HttpPost("account/import")]
         [Consumes("multipart/form-data")]
-        public async Task<ActionResult<List<Account>>> ImportEmplyees(IFormFile file)
+        public async Task<ActionResult<List<Account>>> ImportEmployees(IFormFile file)
         {
             var result = await _excelService.ImportEmployeeList(file);
             return Ok(result);
-        }
-
-        // PUT: api/Excel/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
-        {
-        }
-
-        // DELETE: api/Excel/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
         }
     }
 }
--- CES.BusinessTier/Services/ExcelService.cs
+++ CES.BusinessTier/Services/ExcelService.cs
@@ -1,9 +1,11 @@
 using CES.BusinessTier.RequestModels;
+using CES.BusinessTier.ResponseModels.BaseResponseModels;
 using CES.BusinessTier.UnitOfWork;
 using CES.BusinessTier.Utilities;
 using CES.DataTier.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
 using OfficeOpenXml;
 using OfficeOpenXml.Style;
 
@@ -11,32 +13,27 @@ namespace CES.BusinessTier.Services;
 
 public interface IExcelService
 {
-    Task<List<Account>> ImportEmployeeList(IFormFile file);
+    Task<DynamicResponse<Account>> ImportEmployeeList(IFormFile file);
+    FileStreamResult DownloadEmployeeTemplate();
+    Task<FileStreamResult> DownloadListEmplyeeForCompany();
 }
 
 public class ExcelService : IExcelService
 {
     private readonly IUnitOfWork _unitOfWork;
+    private readonly IHttpContextAccessor _contextAccessor;
 
-    public ExcelService(IUnitOfWork unitOfWork)
+    public ExcelService(IUnitOfWork unitOfWork, IHttpContextAccessor contextAccessor)
     {
         _unitOfWork = unitOfWork;
+        _contextAccessor = contextAccessor;
     }
     
-    public async Task<List<Account>> ImportEmployeeList(IFormFile file)
+    public async Task<DynamicResponse<Account>> ImportEmployeeList(IFormFile file)
     {
+        var companyId = _contextAccessor.HttpContext?.User.FindFirst("CompanyId").Value;
         if (file != null && file.Length > 0)
         {
-            // var fileName = Guid.NewGuid().ToString() + Path.GetExtension(file.FileName);
-            //
-            // var filePath = Path.Combine("uploads", fileName);
-            //
-            // var directory = Path.GetDirectoryName(filePath);
-            // Directory.CreateDirectory(directory);
-            // using (var stream = new FileStream(filePath, FileMode.Create))
-            // {
-            //     await file.CopyToAsync(stream);
-            // }
             ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
             using (var package = new ExcelPackage(file.OpenReadStream()))
             {
@@ -53,23 +50,108 @@ public class ExcelService : IExcelService
                         Email = worksheet.Cells[row, 2].Value?.ToString(),
                         Address = worksheet.Cells[row, 3].Value?.ToString(),
                         Phone = worksheet.Cells[row, 4].Value?.ToString(),
-                        ImageUrl = worksheet.Cells[row, 5].Value?.ToString(),
                         Status = (int)Status.Active,
                         CreatedAt = TimeUtils.GetCurrentSEATime(),
                         RoleId = (int)Roles.Employee,
-                        CompanyId = 1,
+                        CompanyId = int.Parse(companyId),
                         Password = Authen.HashPassword("DefaultPassword")
                     };
 
-                    records.Add(account);
+                    var existAccount = await _unitOfWork.Repository<Account>()
+                        .AsQueryable(x => x.Email == account.Email && x.Status == (int)Status.Active)
+                        .AnyAsync();
+                    if (!existAccount) records.Add(account);
+                }
+
+                foreach (var account in records)
+                {
+                    var wallets = new List<Wallet>()
+                    {
+                        new Wallet
+                        {
+                            AccountId = account.Id,
+                            Balance = 0,
+                            CreatedAt = TimeUtils.GetCurrentSEATime(),
+                            Id = Guid.NewGuid(),
+                            Name = WalletTypeEnums.FoodWallet.GetDisplayName(),
+                            Type = (int)WalletTypeEnums.FoodWallet,
+                        },
+                        new Wallet
+                        {
+                            AccountId = account.Id,
+                            Balance = 0,
+                            CreatedAt = TimeUtils.GetCurrentSEATime(),
+                            Id = Guid.NewGuid(),
+                            Name = WalletTypeEnums.StationeryWallet.GetDisplayName(),
+                            Type = (int)WalletTypeEnums.StationeryWallet,
+                        }
+                    };
+                    account.Wallets = wallets;
                 }
 
                 await _unitOfWork.Repository<Account>().AddRangeAsync(records);
                 await _unitOfWork.CommitAsync();
 
-                return records;
+                return new DynamicResponse<Account>()
+                {
+                    Code = StatusCodes.Status200OK,
+                    Message = "Ok",
+                    Data = records
+                };
             }
         }
-        return new List<Account>();
+
+        return new DynamicResponse<Account>()
+        {
+            Code = StatusCodes.Status200OK,
+            Message = "Ok",
+            Data = new List<Account>()
+        };
+    }
+
+    public FileStreamResult DownloadEmployeeTemplate()
+    {
+        ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+        using (ExcelPackage package = new ExcelPackage())
+        {
+            ExcelWorksheet ws = package.Workbook.Worksheets.Add("Company");
+            ws.Cells["A1"].Value = "Name*";
+            ws.Cells["A1"].Style.Font.Bold = true;
+            ws.Cells["A1"].Style.Font.Size = 16;
+            ws.Cells["A1"].Style.VerticalAlignment = ExcelVerticalAlignment.Center;
+            ws.Cells["A1"].Style.HorizontalAlignment = ExcelHorizontalAlignment.Center;
+            
+            ws.Cells["B1"].Value = "Email*";
+            ws.Cells["B1"].Style.Font.Bold = true;
+            ws.Cells["B1"].Style.Font.Size = 16;
+            ws.Cells["B1"].Style.VerticalAlignment = ExcelVerticalAlignment.Center;
+            ws.Cells["B1"].Style.HorizontalAlignment = ExcelHorizontalAlignment.Center;
+            
+            ws.Cells["C1"].Value = "Address";
+            ws.Cells["C1"].Style.Font.Bold = true;
+            ws.Cells["C1"].Style.Font.Size = 16;
+            ws.Cells["C1"].Style.VerticalAlignment = ExcelVerticalAlignment.Center;
+            ws.Cells["C1"].Style.HorizontalAlignment = ExcelHorizontalAlignment.Center;
+            
+            ws.Cells["D1"].Value = "Phone";
+            ws.Cells["D1"].Style.Font.Bold = true;
+            ws.Cells["D1"].Style.Font.Size = 16;
+            ws.Cells["D1"].Style.VerticalAlignment = ExcelVerticalAlignment.Center;
+            ws.Cells["D1"].Style.HorizontalAlignment = ExcelHorizontalAlignment.Center;
+            
+            ws.Cells.AutoFitColumns();
+            
+            var stream = new MemoryStream(package.GetAsByteArray());
+
+            return new FileStreamResult(stream, "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet")
+            {
+                FileDownloadName = "employee_template.xlsx" // Specify the desired file name
+            };
+        }
+    }
+
+    public Task<FileStreamResult> DownloadListEmplyeeForCompany()
+    {
+        throw new NotImplementedException();
     }
 }
