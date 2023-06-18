2c10139073848cec5d68f85e3268e2312d519d6a
Huy Nguyen Anh <93422806+huy-nguyenanh@users.noreply.github.com>
2023-06-18 16:39:00 +0700

    Update WalletResponseModel.cs

--- CES.BusinessTier/ResponseModels/WalletResponseModel.cs
+++ CES.BusinessTier/ResponseModels/WalletResponseModel.cs
@@ -12,6 +12,7 @@ namespace CES.BusinessTier.ResponseModels
         public string? Name { get; set; }
         public int? Type { get; set; }
         public double? Balance { get; set; }
+        public double? Limit { get; set; }
         public Guid? AccountId { get; set; }
     }
 }
