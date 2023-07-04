d4ae39410af78a4aeb1ee73d38c4cdbce03a7abe
Huy Nguyen Anh <93422806+huy-nguyenanh@users.noreply.github.com>
2023-07-04 18:03:35 +0700

    Update UserResponseModel.cs

--- CES.BusinessTier/ResponseModels/UserResponseModel.cs
+++ CES.BusinessTier/ResponseModels/UserResponseModel.cs
@@ -10,5 +10,7 @@ public class UserResponseModel
     public int? Status { get; set; }
     public DateTime? CreatedAt { get; set; }
     public DateTime? UpdatedAt { get; set; }
-    
+
+    public AccountResponseModel? Account { get; set; }
+
 }
