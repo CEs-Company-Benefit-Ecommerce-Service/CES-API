4aff4f172426858203b491a9d6afda4a06f68b67
Lê Anh Khoa (Kieran) <lakhoa2806@gmail.com>
2023-07-06 23:53:19 +0700

    Update WalletServices.cs

--- CES.BusinessTier/Services/WalletServices.cs
+++ CES.BusinessTier/Services/WalletServices.cs
@@ -291,11 +291,8 @@ namespace CES.BusinessTier.Services
             if (dateTimeOffset <= currentDateTimeOffset)
             {
                 dateTimeOffset = currentDateTimeOffset.AddMinutes(2);
-                dateTimeOffset = dateTimeOffset.AddHours(7);
             }
             BackgroundJob.Schedule(() => UpdateWalletBalanceForGroupAsync(request, accountLoginId), dateTimeOffset);
-            DateTimeOffset systemTimeOffSet = new DateTimeOffset(DateTime.Now);
-            BackgroundJob.Schedule(() => Console.Write($"System Time: {systemTimeOffSet}"), systemTimeOffSet);
             // RecurringJob.AddOrUpdate(() => UpdateWalletBalanceForGroupAsync(request, accountLoginId), TimeUtils.ToCronExpression(time), TimeZoneInfo.FindSystemTimeZoneById("SE Asia Standard Time"));
         }
 
