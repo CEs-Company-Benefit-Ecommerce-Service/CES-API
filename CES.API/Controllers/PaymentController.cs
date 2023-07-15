1d861507b50b3a43430e86fd777ab229e3ee773e
Huy Nguyen Anh <93422806+huy-nguyenanh@users.noreply.github.com>
2023-07-15 14:24:54 +0700

    feat: after payment

---
+++ CES.API/Controllers/PaymentController.cs
@@ -0,0 +1,25 @@
+﻿using CES.BusinessTier.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CES.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PaymentController : ControllerBase
+    {
+        private readonly IDebtServices _debtService;
+
+        public PaymentController(IDebtServices debtServices)
+        {
+            _debtService = debtServices;
+        }
+
+        [HttpGet("{companyId}")]
+        public async Task<ActionResult> GetValuePayment(int companyId)
+        {
+            var result = _debtService.GetValueForPayment(companyId).Result;
+            return StatusCode((int)result.Code, result);
+        }
+    }
+}
